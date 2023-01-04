843287c0b2670eba06573b3888771fdd92a7220d
Dispy-Desktop <tpps89305@hotmail.com>
2023-01-05 00:37:53 +0800

    Add parkour demo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flame_exercise/circles_example/circles_collision_page.dart';
 import 'package:flame_exercise/drag_event_demo/drag_event_page.dart';
 import 'package:flame_exercise/gesture_input_demo/gesture_input_page.dart';
 import 'package:flame_exercise/overlay_demo/overlay_demo_page.dart';
+import 'package:flame_exercise/parkour_example/parkour_page.dart';
 import 'package:flame_exercise/router_demo/router_page.dart';
 import 'package:flame_exercise/tap_event_demo/tap_event_page.dart';
 import 'package:flutter/material.dart';
@@ -71,6 +72,16 @@ class HomePage extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                       builder: (context) => const GestureInputPage()));
+            },
+          ),
+          ListTile(
+            title: const Text("捲軸式跑酷場景"),
+            subtitle: const Text("《人行道生存戰》場景"),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ParkourPage()));
             },
           ),
         ],
---
+++ lib/parkour_example/parkour_page.dart
@@ -0,0 +1,30 @@
+import 'package:flame/game.dart';
+import 'package:flame_exercise/parkour_example/game_component/parkour_game.dart';
+import 'package:flutter/material.dart';
+
+class ParkourPage extends StatelessWidget {
+  const ParkourPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const GameWidget.controlled(gameFactory: ParkourGame.new),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.exit_to_app_rounded,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
