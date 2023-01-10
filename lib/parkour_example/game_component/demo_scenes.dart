5307f99c79e1b64d1170e5c200686f5001610b10
Dispy-Desktop <tpps89305@hotmail.com>
2023-01-11 00:06:44 +0800

    Add scene swith function

--- lib/parkour_example/game_component/demo_scenes.dart
+++ lib/parkour_example/game_component/demo_scenes.dart
@@ -1,21 +1,30 @@
 import 'dart:developer';
+import 'dart:math' hide log;
 
 import 'package:flame/components.dart';
 import 'package:flame/image_composition.dart';
 import 'package:flutter/material.dart';
 
-class DemoSceneOne extends PositionComponent {
-  final _rectPaint = Paint()..color = const Color.fromARGB(135, 219, 198, 224);
+class DemoScene extends PositionComponent {
+  late Paint _rectPaint;
+  final Random random = Random();
+
   @override
   Future<void>? onLoad() async {
     log("Scene one on load", name: "DemoSceneOne");
+    setBackground();
+  }
+
+  @override
+  void onMount() {
+    // 清場，重新佈置場景。
   }
 
   @override
   // ignore: avoid_renaming_method_parameters
   void onGameResize(Vector2 canvasSize) {
     super.onGameResize(canvasSize);
-    size = canvasSize;
+    size = Vector2(500, canvasSize.y);
   }
 
   @override
@@ -26,5 +35,14 @@ class DemoSceneOne extends PositionComponent {
   @override
   void onRemove() {
     log("Scene one on remove", name: "DemoSceneOne");
+  }
+
+  /// 重新設定背景顏色，這裡模擬重新布置場景的情形。
+  setBackground() {
+    int valueR = random.nextInt(256);
+    int valueG = random.nextInt(256);
+    int valueB = random.nextInt(256);
+
+    _rectPaint = Paint()..color = Color.fromARGB(255, valueR, valueG, valueB);
   }
 }
